862abb8b8ec1716a823e961ab5fbebec647b8c03
Luis Saravia <saraviatornero310@gmail.com>
2024-12-23 22:36:42 -0500

    fix(environment): changed "BASE_URL"

--- src/environments/environments.js
+++ src/environments/environments.js
@@ -1,3 +1,4 @@
 export const environments = {
-  BASE_URL: "https://chat-server-production-a60e.up.railway.app"
+  // BASE_URL: "https://chat-server-production-a60e.up.railway.app",
+  BASE_URL: "https://chat-server-1-y76a.onrender.com/"
 }
